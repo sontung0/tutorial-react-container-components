839488537cc1baf5f4743b3db24c00e58643cfc9
sontung0 <sontung0@gmail.com>
2017-05-19 20:36:18 +0700

    Update

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,11 @@
 import React, {Component} from 'react';
 import CommentListContainer from '../containers/CommentListContainer';
 
-class App extends Component {
-    render() {
-        return (
-            <div className="container">
-                <h1>Comments</h1>
-                <CommentListContainer/>
-            </div>
-        );
-    }
-}
+const App = () => (
+    <div className="container">
+        <h1>Comments</h1>
+        <CommentListContainer/>
+    </div>
+);
 
 export default App;
